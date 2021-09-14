5626dbc59ce61eb3472c73af1cfcabeb1b665fb6
mahadirazib <31607628+mahadirazib@users.noreply.github.com>
2021-09-14 20:22:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-dlet a = prompt("First text: ");
+
+
+
+document.querySelector("#check").addEventListener("click",checkingTag);
+
+
+function checkingTag(){
+let a = document.querySelector("#firstText").value;
 a = a.toLowerCase()
-a = a.replaceAll(".", "");
-a = a.replaceAll(",", "");
-a = a.replaceAll(")", "");
-a = a.replaceAll("(", "");
+a = a.replaceAll(".", "").replaceAll(",", "").replaceAll("!", "").replaceAll("*", " ").replaceAll("?", "").replaceAll(")", "").replaceAll("(", "").replace(/(\r\n|\n|\r)/gm, " ").replaceAll("  ", " ");
 a = a.split(" ");
-let b = prompt("Second text: ");
+
+let b = document.querySelector("#secondText").value;
 b = b.toLowerCase()
-b = b.replaceAll(".", "");
-b = b.replaceAll(",", "");
-b = b.replaceAll(")", "");
-b = b.replaceAll("(", "");
+b = b.replaceAll(".", "").replaceAll(",", "").replaceAll("!", "").replaceAll("*", " ").replaceAll("?", "").replaceAll(")", "").replaceAll("(", "").replace(/(\r\n|\n|\r)/gm, " ").replaceAll("  ", " ");
 b = b.split(" ");
+
+
 let tag = []
 
 let numberb = b.lenght;
@@ -24,7 +28,7 @@ if (numbera<numberb){
     num = numbera;
 }
 
-let ignore = ["to","and","or","but","the","a","an","in","on","at","it","i","will","of","for","is","so","with","if",",",".",":","/","-","_"];
+let ignore = ["to","and","or","but","the","a","an","in","on","at","it","i","me","my","you","your","will","of","for","is","so","with","if",",",".",":","/","-","_"];
 
 for (let i = 0; i < num ; i++) {
     if (b.includes(a[i]) && ignore.includes(a[i])== false){
@@ -32,16 +36,21 @@ for (let i = 0; i < num ; i++) {
     }
 }
 
-var tagNumber = document.createElement("h3");
-  tagNumber.innerText = "Total similar keyword: " + tag.length + ".";
-  document.body.appendChild(tagNumber);
+tag = tag.filter(deleteEmpty);
 
-
-for (let r = 0; r < (tag.length) ; r++) {
-
-  var showtag = document.createElement("p");
-    showtag.innerText =  tag[r] + ", ";
-    document.body.appendChild(showtag);
+function deleteEmpty(a) {
+    return a != "";
 }
 
+
+
+document.querySelector("#textShow").innerHTML = "<h3>Total similar keyword: " + tag.length + ".</h3>" + "<h3>The keywords are:  </h3> <p id='forTag'>  </p> ";
+document.querySelector("#forTag").innerText = tag[0];
+
+for(let i=1;i<tag.length;i++){
+    document.querySelector("#forTag").innerText = document.querySelector("#forTag").innerText + ", " + tag[i] ;
+}
+
+
 console.log(tag);
+}
